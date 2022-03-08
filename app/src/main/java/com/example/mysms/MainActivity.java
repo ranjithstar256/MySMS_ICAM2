13b56b20ce1b9ffa1671e1a812758ddf25c96d51
ctadmin <ranjithstar256@gmail.com>
2022-03-08 08:32:24 +0530

    FIRST

---
+++ app/src/main/java/com/example/mysms/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.mysms;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView img;
+    Animation animtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img=findViewById(R.id.imageView);
+        animtn= AnimationUtils.loadAnimation(this,R.anim.acb);
+        img.startAnimation(animtn);
+    }
+
+}
